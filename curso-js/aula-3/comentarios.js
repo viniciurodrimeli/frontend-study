797ff8ce986b54c9391125992c998eb8b34bf6d2
viniciurodrimeli <vinicius.vrodrigues@mercadolivre.com>
2022-04-16 14:06:45 -0300

    aula 3 - comentarios

---
+++ curso-js/aula-3/comentarios.js
@@ -0,0 +1,9 @@
+// E escrever um comentario, anotacao no codigo
+console.log('Olá mundo!'); // outro comentario
+console.log('Linha 1');
+// console.log('Linha 2');
+/*
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+// Vai ser ignorado
